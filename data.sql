2f8468a9f6833c5be1554392a4d2848c09dec6c7
ikirray <95755148+ikirray@users.noreply.github.com>
2022-02-01 19:52:44 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,3 +2,15 @@ insert into usr (first_name, middle_name, last_name, email) values
 ('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
 ('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
 ('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
